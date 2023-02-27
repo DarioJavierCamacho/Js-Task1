fd007c4491fdfa0cd42ef01b98b5710261e44b83
DarioJavierCamacho <Darioj1990@gmail.com>
2023-02-27 14:53:09 -0300

    final

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const fechaActual = data.currentDate;
 const nodoTarjetas = document.getElementById('card-container');
 
 
-function crearTarjetas(arrayData){
+/* Parametros   
+                arrayData:array con los todos los eventos
+                nodo: nodo donde insertar el contenido html
+                
+*/ 
+function crearTarjetas(arrayData,nodo){
     let stringTarjeta = "";
     for(let  dato of arrayData){
         stringTarjeta +=  `<div class="col-sm-11 col-md-5 col-xxl-4 mt-4 mb-4 d-flex justify-content-center">
         <div class="card" style="width: 18rem;">
             <img src="${dato.image}" class="card-img-top" alt="...">
-            <div class="card-body">
+            <div class="card-body d-flex flex-column">
                 <h5 class="card-title">${dato.name}</h5>
-                <p class="card-text">${dato.description}</p>
+                <p class="card-text flex-grow-1">${dato.description}</p>
                 <div class="card-bottom d-flex flex-row justify-content-between">
                     <div>Price $${dato.price}</div>
                     <a href="#" class="btn btn-primary">Ver más...</a>
@@ -20,7 +25,7 @@ function crearTarjetas(arrayData){
         </div>
     </div>`
     }
-    nodoTarjetas.innerHTML = stringTarjeta;
+    nodo.innerHTML = stringTarjeta;
 }
 
-crearTarjetas(datos);
+crearTarjetas(datos,nodoTarjetas);
--- past.js
+++ past.js
@@ -3,27 +3,36 @@ const fechaActual = data.currentDate;
 const nodoTarjetas = document.getElementById('card-container');
 
 
-function filtrarPasado(arrayData){
+/*Parametros    
+                arrayData: array con los eventos
+                fecActual: fecha actual con la cual comparar los eventos
+*/ 
+
+
+function filtrarPasado(arrayData,fecActual){
     let arrayPasado = [];
     for(let dato of arrayData){    
-        if(dato.date < fechaActual)
+        if(dato.date < fecActual)
         arrayPasado.push(dato);
     }
     return arrayPasado;
 }
 
 
+/* Parametros   
+                nodo: nodo donde insertar el contenido html
+                pasados:array con los eventos pasados
+*/ 
 
-function crearTarjetas(arrayData){
-    let pasados= filtrarPasado(arrayData);
+function crearTarjetas(nodo,pasados){ 
     let stringTarjeta = "";
     for(let  pasado of pasados){
         stringTarjeta +=  `<div class="col-sm-11 col-md-5 col-xxl-4 mt-4 mb-4 d-flex justify-content-center">
         <div class="card" style="width: 18rem;">
             <img src="${pasado.image}" class="card-img-top" alt="...">
-            <div class="card-body">
+            <div class="card-body d-flex flex-column">
                 <h5 class="card-title">${pasado.name}</h5>
-                <p class="card-text">${pasado.description}</p>
+                <p class="card-text flex-grow-1">${pasado.description}</p>
                 <div class="card-bottom d-flex flex-row justify-content-between">
                     <div>Price $${pasado.price}</div>
                     <a href="#" class="btn btn-primary">See More...</a>
@@ -32,7 +41,8 @@ function crearTarjetas(arrayData){
         </div>
     </div>`
     }
-    nodoTarjetas.innerHTML = stringTarjeta;
+    nodo.innerHTML = stringTarjeta;
 }
 
-crearTarjetas(datos);
+let arrayDatos = filtrarPasado(datos,fechaActual)
+crearTarjetas(nodoTarjetas,arrayDatos);
